85bd39a973133500c4392f567ade5629b331802f
walterdevreis <walter-reis@outlook.com>
2023-12-19 15:34:31 -0300

    criado o metodo atacar

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 
 class heroi{
-
+    
     constructor(nome, idade, tipo){
         this.nome = nome 
         this.idade = idade
@@ -9,6 +9,13 @@ class heroi{
     }
 
     atacar(){
-        
+        if(this.tipo === "mago")
+            console.log(`O ${this.tipo} atacou usando magia`)
+        else if(this.tipo === "guerreiro")
+            console.log(`O ${this.tipo} atacou usando espada`)
+        else if(this.tipo === "monge")
+            console.log(`O ${this.tipo} atacou usando artes marciais`)
+        else if(this.tipo === "ninha")
+            console.log(`O ${this.tipo} atacou usando shuriken`)
     }
 }
